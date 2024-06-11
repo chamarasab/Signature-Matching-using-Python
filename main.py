dabe513c0a644c04e048529c021e8793b0e33a13
Chamara Ekanayake <90494177+chamarasab@users.noreply.github.com>
2024-06-11 06:03:30 +0000

    sample images

--- main.py
+++ main.py
@@ -46,9 +46,9 @@ def capture_image_from_cam_into_temp(sign=1):
                 os.mkdir('temp', mode=0o777)  # make sure the directory exists
             # img_name = "./temp/opencv_frame_{}.png".format(img_counter)
             if(sign == 1):
-                img_name = "./temp/test_img1.png"
+                img_name = "signatures/IMG_2276.jpeg"
             else:
-                img_name = "./temp/test_img2.png"
+                img_name = "signatures/IMG_2277.jpeg"
             print('imwrite=', cv2.imwrite(filename=img_name, img=frame))
             print("{} written!".format(img_name))
             # img_counter += 1
